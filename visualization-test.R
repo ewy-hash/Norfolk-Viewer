6ce8b47d0b5181b891fcff31a3817f5fb6875ae6
ewy-hash <ewyers@students.pitzer.edu>
2025-12-02 14:19:36 -0800

    added smaller sample in vis test

--- visualization-test.R
+++ visualization-test.R
@@ -11,6 +11,22 @@ library(rgl)
 rast.small5 <- rast("Data/raster-small5.tif")
 rast.small5 <- raster::raster(rast.small5)
 
+unclean.norf <- rast("Data/VA_Southern_GCS_3m_NAVDm.tif")
+
+bbox <- ext(c(xmin = -76.392655, xmax = -76.388909, ymin = 36.832284, ymax = 36.835033))
+small.sample.rast <- mask(unclean.norf, bbox)
+small.sample.rast <- trim(small.sample.rast)
+plot(small.sample.rast)
+small.sample.rast <- raster::raster(small.sample.rast)
+plot3D(small.sample.rast, maxpixels = 1e7)
+
+rast.small <- rast("Data/unclean-norf-trimmed.tif")
+rast.small <- raster::raster(rast.small)
+plot3D(rast.small, zfac = 0.1, maxpixels = 1e6)
+
+
+
+
 rast.big <- rast("Data/VA_Southern_GCS_3m_NAVDm.tif")
 rast.big5 <- terra::aggregate(rast.big, fact = 5)
 rast.big5 <- raster::raster(rast.big5)
